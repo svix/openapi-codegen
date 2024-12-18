892d100e665bae18bd60483b2648e27ac8ae57f1
Jonas Platte <jplatte@svix.com>
2024-12-18 11:04:54 +0100

    Add go formatting
    … and log formatting errors.

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::{collections::BTreeSet, io, process::Command, sync::Mutex};
+
 use camino::Utf8Path;
 
 pub(crate) fn get_schema_name(maybe_ref: Option<String>) -> Option<String> {
@@ -17,10 +19,33 @@ pub(crate) fn run_formatter(path: &Utf8Path) {
         return;
     };
 
-    if file_ext == "rs" {
-        _ = std::process::Command::new("rustfmt")
-            .args(["+nightly", "--edition", "2021"])
-            .arg(path)
-            .status();
+    let (formatter, args) = match file_ext {
+        "rs" => ("rustfmt", ["+nightly", "--edition", "2021"].as_slice()),
+        "go" => ("gofmt", [].as_slice()),
+        _ => {
+            tracing::debug!("no known formatter for {file_ext} files");
+            return;
+        }
+    };
+
+    let result = Command::new(formatter).args(args).arg(path).status();
+    match result {
+        Ok(exit_status) if exit_status.success() => {}
+        Ok(exit_status) => {
+            tracing::warn!(exit_status = exit_status.code(), "`{formatter}` failed");
+        }
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {
+            // only print one error per formatter that's not found
+            static NOT_FOUND_LOGGED_FOR: Mutex<BTreeSet<&str>> = Mutex::new(BTreeSet::new());
+            if NOT_FOUND_LOGGED_FOR.lock().unwrap().insert(formatter) {
+                tracing::warn!("`{formatter}` not found");
+            }
+        }
+        Err(e) => {
+            tracing::warn!(
+                error = &e as &dyn std::error::Error,
+                "running `{formatter}` failed"
+            );
+        }
     }
 }
