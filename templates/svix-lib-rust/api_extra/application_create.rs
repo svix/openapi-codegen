3d0eb1f629e2c03fc1ee1a96f81a1b54fd98e8a7
Jonas Platte <jplatte@svix.com>
2025-01-22 14:22:10 +0100

    rust: Integrate header parameters into options structs

--- templates/svix-lib-rust/api_extra/application_create.rs
+++ templates/svix-lib-rust/api_extra/application_create.rs
@@ -3,14 +3,14 @@
 pub async fn get_or_create(
     &self,
     application_in: ApplicationIn,
-    options: Option<PostOptions>,
+    options: Option<ApplicationCreateOptions>,
 ) -> Result<ApplicationOut> {
-    let PostOptions { idempotency_key } = options.unwrap_or_default();
+    let ApplicationCreateOptions { idempotency_key } = options.unwrap_or_default();
 
     crate::request::Request::new(http1::Method::POST, "/api/v1/app")
-        .with_body_param(application_in)
         .with_query_param("get_if_exists", "true".to_owned())
         .with_optional_header_param("idempotency-key", idempotency_key)
+        .with_body_param(application_in)
         .execute(self.cfg)
         .await
 }
