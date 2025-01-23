afcc7edac03614d1598f955ce02e523be5462e9b
Mendy <mman@svix.com>
2025-01-23 08:43:45 -0500

    rust: This is not git2 anymore
    Co-authored-by: Jonas Platte <jplatte@svix.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() {
-    let git2 = GitclBuilder::default().sha(false).build().unwrap();
+    let git = GitclBuilder::default().sha(false).build().unwrap();
     Emitter::new()
-        .add_instructions(&git2)
+        .add_instructions(&git)
         .unwrap()
         .emit()
         .unwrap();
