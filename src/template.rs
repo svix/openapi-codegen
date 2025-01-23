cb0fce6b8c8327436518fbf69979b1a14107bd13
Jonas Platte <jplatte@svix.com>
2025-01-23 14:25:14 +0100

    Add has[_required]_query_or_header_params filters

--- src/template.rs
+++ src/template.rs
@@ -3,13 +3,15 @@ use std::borrow::Cow;
 use camino::Utf8Path;
 use heck::{ToLowerCamelCase as _, ToSnakeCase as _, ToUpperCamelCase as _};
 use itertools::Itertools as _;
-use minijinja::{path_loader, value::Kwargs};
+use minijinja::{path_loader, value::Kwargs, Value};
 
 pub(crate) fn env(tpl_dir: &Utf8Path) -> Result<minijinja::Environment<'static>, minijinja::Error> {
     let mut env = minijinja::Environment::new();
     env.set_loader(path_loader(tpl_dir));
 
-    // Custom filters
+    // === Custom filters ===
+
+    // --- Case conversion ---
     env.add_filter("to_snake_case", |s: Cow<'_, str>| s.to_snake_case());
     env.add_filter("to_lower_camel_case", |s: Cow<'_, str>| {
         s.to_lower_camel_case()
@@ -17,6 +19,26 @@ pub(crate) fn env(tpl_dir: &Utf8Path) -> Result<minijinja::Environment<'static>,
     env.add_filter("to_upper_camel_case", |s: Cow<'_, str>| {
         s.to_upper_camel_case()
     });
+
+    // --- OpenAPI utils ---
+    env.add_filter(
+        "has_query_or_header_params",
+        |operation: Value| -> Result<bool, minijinja::Error> {
+            let query_params = operation.get_attr("query_params")?;
+            let header_params = operation.get_attr("header_params")?;
+            Ok(query_params.len() > Some(0) || header_params.len() > Some(0))
+        },
+    );
+    env.add_filter(
+        "has_required_query_or_header_params",
+        |operation: Value| -> Result<bool, minijinja::Error> {
+            let query_params = operation.get_attr("query_params")?;
+            let header_params = operation.get_attr("header_params")?;
+            Ok(contains_required_param(query_params)? || contains_required_param(header_params)?)
+        },
+    );
+
+    // --- Comment generation ---
     env.add_filter(
         "to_doc_comment",
         |s: Cow<'_, str>, kwargs: Kwargs| -> Result<String, minijinja::Error> {
@@ -67,4 +89,14 @@ pub(crate) fn env(tpl_dir: &Utf8Path) -> Result<minijinja::Environment<'static>,
     );
 
     Ok(env)
+}
+
+fn contains_required_param(value: Value) -> Result<bool, minijinja::Error> {
+    for p in value.try_iter()? {
+        if p.get_attr("required")?.is_true() {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
 }
