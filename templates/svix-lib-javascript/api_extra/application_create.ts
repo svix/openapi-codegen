86644203de6cb9e7c5cafcaf77e5978001c98c14
Jonas Platte <jplatte@svix.com>
2025-01-29 16:01:58 +0100

    js: Update api_extra indentation to two spaces

--- templates/svix-lib-javascript/api_extra/application_create.ts
+++ templates/svix-lib-javascript/api_extra/application_create.ts
@@ -1,13 +1,13 @@
 /** Get the application with the UID from `applicationIn`, or create it if it doesn't exist yet. */
 public getOrCreate(
-    applicationIn: ApplicationIn,
-    options?: PostOptions
+  applicationIn: ApplicationIn,
+  options?: PostOptions
 ): Promise<ApplicationOut> {
-    const request = new SvixRequest(HttpMethod.POST, "/api/v1/app");
+  const request = new SvixRequest(HttpMethod.POST, "/api/v1/app");
 
-    request.body = applicationIn;
-    request.setQueryParam("get_if_exists", true);
-    request.setHeaderParam("idempotency-key", options?.idempotencyKey);
+  request.body = applicationIn;
+  request.setQueryParam("get_if_exists", true);
+  request.setHeaderParam("idempotency-key", options?.idempotencyKey);
 
-    return request.send(this.requestCtx);
+  return request.send(this.requestCtx);
 }
