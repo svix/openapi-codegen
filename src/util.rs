f3e10d700c9ddfe87db90d4c0462283a334ae676
Mendy <mman@svix.com>
2025-01-29 09:28:16 -0500

    Refactor post-processing to allow for a single postprocessing step (#42)

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,6 @@
 use sha2::{Digest, Sha256};
 use std::collections::BTreeMap;
-use std::{collections::BTreeSet, io, process::Command, sync::Mutex};
 
-use camino::Utf8Path;
 use serde::ser::{Serialize, SerializeSeq as _, Serializer};
 
 pub(crate) fn get_schema_name(maybe_ref: Option<&str>) -> Option<String> {
@@ -15,66 +13,6 @@ pub(crate) fn get_schema_name(maybe_ref: Option<&str>) -> Option<String> {
         );
     };
     Some(schema_name?.to_owned())
-}
-
-pub(crate) fn run_postprocessing(path: &Utf8Path) {
-    let Some(file_ext) = path.extension() else {
-        return;
-    };
-
-    let postprocessing_tasks: &[(&str, &[&str])] = {
-        match file_ext {
-            "py" => &[
-                // fixme: the ordering of the commands is important, maybe ensure the order in a more robust way
-                ("ruff", ["check", "--fix"].as_slice()), // First lint and remove unused imports
-                ("ruff", ["check", "--select", "I", "--fix"].as_slice()), // Then sort imports
-                ("ruff", ["format"].as_slice()),         // Then format the file
-            ],
-            "rs" => &[(
-                "rustfmt",
-                [
-                    "+nightly",
-                    "--unstable-features",
-                    "--skip-children",
-                    "--edition",
-                    "2021",
-                ]
-                .as_slice(),
-            )],
-            "go" => &[("gofmt", ["-w"].as_slice())],
-            "kt" => &[("ktfmt", ["--kotlinlang-style"].as_slice())],
-            _ => {
-                tracing::debug!("no known postprocessing command(s) for {file_ext} files");
-                return;
-            }
-        }
-    };
-    for (command, args) in postprocessing_tasks {
-        execute_postprocessing_command(path, command, args);
-    }
-}
-
-fn execute_postprocessing_command(path: &Utf8Path, command: &'static str, args: &[&str]) {
-    let result = Command::new(command).args(args).arg(path).status();
-    match result {
-        Ok(exit_status) if exit_status.success() => {}
-        Ok(exit_status) => {
-            tracing::warn!(exit_status = exit_status.code(), "`{command}` failed");
-        }
-        Err(e) if e.kind() == io::ErrorKind::NotFound => {
-            // only print one error per command that's not found
-            static NOT_FOUND_LOGGED_FOR: Mutex<BTreeSet<&str>> = Mutex::new(BTreeSet::new());
-            if NOT_FOUND_LOGGED_FOR.lock().unwrap().insert(command) {
-                tracing::warn!("`{command}` not found");
-            }
-        }
-        Err(e) => {
-            tracing::warn!(
-                error = &e as &dyn std::error::Error,
-                "running `{command}` failed"
-            );
-        }
-    }
 }
 
 pub(crate) fn serialize_btree_map_values<K, V, S>(
