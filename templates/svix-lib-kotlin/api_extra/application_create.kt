0e5c30bf85a994bd79c2feee57bf7b2bfdd8c054
Mendy Man <mman@svix.com>
2025-02-12 22:13:35 -0500

    kotlin: Refactor to not inherit from SvixHttpClient

--- templates/svix-lib-kotlin/api_extra/application_create.kt
+++ templates/svix-lib-kotlin/api_extra/application_create.kt
@@ -3,11 +3,11 @@ suspend fun getOrCreate(
     applicationIn: ApplicationIn,
     options: ApplicationCreateOptions = ApplicationCreateOptions()
 ): ApplicationOut {
-    val url = this.newUrlBuilder().encodedPath("/api/v1/app").addQueryParameter("get_if_exists","true")
+    val url = client.newUrlBuilder().encodedPath("/api/v1/app").addQueryParameter("get_if_exists","true")
     var headers = Headers.Builder()
     options.idempotencyKey?.let { headers = headers.add("idempotency-key", it) }
 
-    return this.executeRequest<ApplicationIn, ApplicationOut>(
+    return client.executeRequest<ApplicationIn, ApplicationOut>(
         "POST",
         url.build(),
         headers = headers.build(),
